481d656fb1fecd03e4640ae87d5c1de3c32e4e93
Amalia <74189129+chocolateprincess666@users.noreply.github.com>
2021-11-11 17:49:54 +0100

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';//new change
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -9,6 +10,18 @@ import { SPAsComponent } from './spas/spas.component';
 import { ArtitekturAngularComponent } from './artitektur-angular/artitektur-angular.component';
 import { VadTypescriptArComponent } from './vad-typescript-ar/vad-typescript-ar.component';
 import { SpaVsMultiComponent } from './spa-vs-multi/spa-vs-multi.component';
+//import { RouterModule } from '@angular/router';
+
+//new change
+const routes: Routes = [
+  {path: 'min-profil', component: MinProfilComponent},
+  {path: 'angular', component: VadAngularArComponent},
+  {path: 'spa', component: SPAsComponent},
+  {path: 'ang-arki', component: ArtitekturAngularComponent},
+  {path: 'typescript', component: VadTypescriptArComponent},
+  {path: 'spaVsmulti', component: SpaVsMultiComponent},
+  {path: '', redirectTo: '/min-profil', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
@@ -22,7 +35,8 @@ import { SpaVsMultiComponent } from './spa-vs-multi/spa-vs-multi.component';
     SpaVsMultiComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)//new change
   ],
   providers: [],
   bootstrap: [AppComponent]
